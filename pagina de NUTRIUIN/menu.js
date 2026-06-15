9dd6b7ac31acc901761a2dad8f82ee43b5cb78ee
joelgutierrezl-cloud <joel.gutierrez.l@uinenlinea.mx>
2026-06-15 10:39:50 -0600

    Add files via upload

---
+++ pagina de NUTRIUIN/menu.js
@@ -0,0 +1,18 @@
+/*
+=====================================================
+FUNCIÓN IR FORMULARIO
+=====================================================
+
+Abre la página donde el usuario
+capturará sus datos nutricionales.
+*/
+
+function irFormulario(){
+
+    /*
+    Cambia a la página mvp.html
+    */
+
+    window.location.href = "mvp.html";
+
+}
